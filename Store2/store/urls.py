af763e5ddead748a34816ea5541a0fc7f84d1447
NurudeenNajeem <najeemnurudeen1990@gmail.com>
2023-06-20 07:57:41 +0100

    my first commit

---
+++ Store2/store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+     path('products/', views.ProductList.as_view()),
+     path('products/<int:id>/', views.ProductDetail.as_view()),
+     # path('collections/', views.CollectionList.as_view(),name = "collection-detail"),
+
+     path('collections/<int:pk>/', views.collection_detail,name = "collection-detail"),
+     path('collections/', views.collection_list),
+
+
+]
